bf4ff93721617ad23617a0d3ccbea6a57ae103d8
Jayandran Sampath <jayandran@Jayandrans-Mac-mini.local>
2024-09-18 00:15:15 +0100

    updated bot code

---
+++ README.md
@@ -0,0 +1,13 @@
+command to execute
+
+first create virtual env
+
+python3 -m venv .venv
+
+source ./.venv/bin/activate
+
+pip install -r requirements.txt
+
+export BOT_TOKEN=fsdfsadf
+
+python3 bot.py
--- bot.py
+++ bot.py
@@ -1,19 +1,66 @@
 # This Bot is created to Serve Dragon On Tron - $DGTRON
 import os
-
 import telebot
+import urllib.error
+from dotenv import load_dotenv
+import urllib.request
+import ssl
+import json
+import jsonpath_ng as jp
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+load_dotenv()
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+if(BOT_TOKEN is None):
+	print("No BOT token")
+	exit
+TRON_SCAN_URL="https://apilist.tronscanapi.com/api/search/v2?term="+os.getenv('DGTRON_ADDRESS')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+PRICE_VALUE = 0.0
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("SunSwap", url="https://sun.io/#/sun_swap/v2?t0=T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb&t1=TPBEsjyW8gZ72wpkyBF7gQNszKpGnSQT8e&type=swap"),
+                               InlineKeyboardButton("SunPump", url="https://sunpump.meme/token/TPBEsjyW8gZ72wpkyBF7gQNszKpGnSQT8e"))
+    return markup
+
+def get_latest_Price():
+    try:
+        response = urllib.request.urlopen(TRON_SCAN_URL)
+        contents = response.read()
+        json_object = json.loads(contents)
+        name_query = jp.parse("$.token[0].market_info.priceInUsd")
+        result = name_query.find(json_object)
+        PRICE_VALUE = result[0].value
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
+    finally:return PRICE_VALUE
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing? Welcome To Dragon On Tron $DGTRON")
 
-@bot.message_handler(commands=['ca', 'contract_address', 'CA', 'Contract_Address', 'CONTRACT_ADDRESS'])
+@bot.message_handler(commands=['ca','buy','hodl','purchase','howtobuy','dgtron','$dgtron','CA','BUY','HODL','PURCHASE','HOWTOBUY','DGTRON','$DGTRON','contract_address', 'Contract_Address', 'CONTRACT_ADDRESS'])
 def send_welcome(message):
-    bot.reply_to(message, "https://tronscan.org/#/token20/TPBEsjyW8gZ72wpkyBF7gQNszKpGnSQT8e")
+    PRICE_VALUE = get_latest_Price()
+    chatID = message.chat.id
+    bot.send_message(chatID,'''
+	DRAGON on TRON | DGTRON | TPBEsjyW8gZ72wpkyBF7gQNszKpGnSQT8e
+				  
+				  
+	Listed on SunSwap ✅
+				  
+				  
+	Price: ${:.9f}
+				  
+				  '''.format(PRICE_VALUE),reply_markup=gen_markup())
 
 @bot.message_handler(commands=['when', 'WHEN'])
 def send_welcome(message):
@@ -21,7 +68,7 @@ def send_welcome(message):
     "耐心是成功的关键 \n" +
     "la paciencia es la clave del éxito \n" +
     "धैर्य सफलता की कुंजी है \n" +
-    "பொறுமையே வெற்றிக்கு முக்கியமாகும் \n" +
+    "பொறுத்தார் பூமி ஆள்வார்... \n" +
     "సహనం విజయానికి కీలకం \n" +
     "kannatlikkus on edu võti \n" +
     "терпение – ключ к успеху \n" +
